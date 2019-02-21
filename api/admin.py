ba2b1337284fc5d147fef06d14d90347258ceae3
Rashmi Agnihotri <agnihotrirashmi20@gmail.com>
2019-02-21 22:49:17 +0530

    Initial commit

---
+++ api/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Items
+admin.site.register(Items)
+
+
+# Register your models here.
